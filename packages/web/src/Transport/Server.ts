f6e1765fce19c8d72ff5b3e2af2e210772958084
Artur Bier <izatop@gmail.com>
2020-03-26 01:42:20 +0300

    fix: web server fix

--- packages/web/src/Transport/Server.ts
+++ packages/web/src/Transport/Server.ts
@@ -1,40 +1,47 @@
 import {Application} from "@typesafeunit/app";
+import {IDisposable} from "@typesafeunit/unit";
 import * as http from "http";
 import {Connection} from "./Connection";
 
-export class Server {
+export class Server implements IDisposable {
+    protected readonly server: http.Server;
     protected readonly application: Application<any, any>;
 
     constructor(application: Application<any, any>) {
         this.application = application;
+        this.server = new http.Server();
     }
 
     public listen(port: number) {
-        const server = new http.Server(async (req, res) => {
-            try {
-                const transport = new Connection(req, res);
-                await this.application.handle(transport);
-            } catch (error) {
-                if (!res.headersSent) {
-                    res.writeHead(500, "Internal Server Error");
+        if (this.server.listening) {
+            return this;
+        }
+
+        this.server.on(
+            "request",
+            async (req, res) => {
+                try {
+                    await this.application.handle(new Connection(req, res));
+                } catch (error) {
+                    if (!res.headersSent) {
+                        res.writeHead(500, "Internal Server Error");
+                    }
+                } finally {
+                    res.writable && res.end();
                 }
-            } finally {
-                res.writable && res.end();
-            }
-        });
+            })
+            .listen(port);
 
-        server.listen(port);
-
-        this.destroyServerCallback = () => new Promise((res, rej) => (
-            server.close((error) => error && rej(error) || res())
-        ));
-
-        return this.destroyServerCallback;
+        return this;
     }
 
     public close() {
-        return this.destroyServerCallback();
+        return new Promise((resolve) => this.server.close(() => resolve));
     }
 
-    private destroyServerCallback = () => Promise.resolve();
+    public async dispose() {
+        if (this.server.listening) {
+            await this.close();
+        }
+    }
 }
