9ab3d7e8cb8b9ccca11fd3e0ab3b9fa2be999abf
Artur Bier <izatop@gmail.com>
2020-06-04 13:50:18 +0300

    fix: stream

--- packages/util/src/stream.ts
+++ packages/util/src/stream.ts
@@ -1,38 +1,35 @@
 import {Duplex, Readable, Stream, Transform, Writable} from "stream";
+import {isObject} from "./is";
 
 type IsStream = Readable | Writable | Duplex | Transform | Stream;
 
-const isStream = (stream: any): stream is IsStream => (
+const isStream = (stream: unknown): stream is IsStream => (
     stream !== null &&
-    typeof stream === "object" &&
+    isObject(stream) &&
     typeof stream.pipe === "function"
 );
 
-const isWritableStream = (stream: any): stream is Writable => (
+const isWritableStream = (stream: unknown): stream is Writable => (
     isStream(stream) &&
     "writable" in stream &&
-    stream.writable !== false &&
     typeof stream._write === "function"
 );
 
-const isReadableStream = (stream: any): stream is Readable => (
+const isReadableStream = (stream: unknown): stream is Readable => (
     isStream(stream) &&
     "readable" in stream &&
-    stream.readable !== false &&
     typeof stream._read === "function"
 );
 
-const isDuplexStream = (stream: any): stream is Duplex => (
+const isDuplexStream = (stream: unknown): stream is Duplex => (
     isWritableStream(stream) &&
     isReadableStream(stream)
 );
 
-const isTransformStream = (stream: any): stream is Transform => (
+const isTransformStream = (stream: unknown): stream is Transform => (
     isStream(stream) &&
     "readable" in stream &&
-    stream.readable !== false &&
     "writable" in stream &&
-    stream.writable !== false &&
     "_transform" in stream &&
     typeof stream._transform === "function"
 );
