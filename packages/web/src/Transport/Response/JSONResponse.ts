8a722c860897f8af8cb41c603448d33d1894a308
Artur Bier <izatop@gmail.com>
2020-08-16 10:12:46 +0300

    fix: lint

--- packages/web/src/Transport/Response/JSONResponse.ts
+++ packages/web/src/Transport/Response/JSONResponse.ts
@@ -1,9 +1,9 @@
 import {ResponseAbstract} from "./ResponseAbstract";
 
-export class JSONResponse extends ResponseAbstract<any> {
+export class JSONResponse<T extends any> extends ResponseAbstract<T> {
     public readonly type = "application/json";
 
-    public stringify(data: any): string {
+    public stringify(data: T): string {
         return JSON.stringify(data);
     }
 }
