9762383c022102de081f4c68af67105e586ca791
abir <izatop@gmail.com>
2020-10-30 15:29:11 +0300

    fix: update deps, validation child

--- packages/unit/src/Validation/ValidationChild.ts
+++ packages/unit/src/Validation/ValidationChild.ts
@@ -1,4 +1,5 @@
 import {ValidationSchema} from "./ValidationSchema";
 
-export class ValidationChild<T extends Record<any, any>, K extends keyof T> extends ValidationSchema<T[K]> {
+export class ValidationChild<T extends Record<any, any>, K extends keyof T>
+    extends ValidationSchema<Exclude<T[K], null | undefined>> {
 }
